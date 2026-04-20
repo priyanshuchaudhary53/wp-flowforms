25c31ae20b74dbc89959de6527e8f8839a291f5e
Priyanshu Chaudhary <priyanshuchaudhary53@gmail.com>
2026-04-21 03:13:40 +0530

    Update plugin URI and add Pro URL for improved access to features

--- flowforms.php
+++ flowforms.php
@@ -7,7 +7,7 @@
  *
  * Plugin Name:       FlowForms
  * Description:       FlowForms lets you create beautiful step-by-step forms in WordPress. A modern conversational form builder and Typeform alternative for WordPress.
- * Plugin URI:        https://flowforms.priyanshuc.dev/
+ * Plugin URI:        https://wpflowforms.com/
  * Version:           1.0.0
  * Requires at least: 6.2
  * Author:            Priyanshu
@@ -27,6 +27,8 @@ define('FLOWFORMS_SLUG', 'flowforms');
 define('FLOWFORMS_FILE', __FILE__);
 define('FLOWFORMS_PATH', plugin_dir_path(__FILE__));
 define('FLOWFORMS_URL', plugin_dir_url(__FILE__));
+
+define('FLOWFORMS_PRO_URL', 'https://wpflowforms.com/pro');
 
 // Autoload classes
 require_once FLOWFORMS_PATH . 'includes/FlowForms.php';
